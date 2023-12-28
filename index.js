dba2ae59437f0acf45465e5307c4053cfa8848e5
Dhakshin A V <avdhakshin1354@gmail.com>
2023-12-28 18:50:20 +0530

    modified:   index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import ReviewsDAO from "./dao/reviewsDAO.js"
+
+const MongoClient =mongodb.MongoClient
+const mongo_username = process.env['MONGO_USERNAME']
+const mongo_password = process.env['MONGO_PASSWORD']
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.uh0hgvc.mongodb.net/?retryWrites=true&w=majority`
